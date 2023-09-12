76d6416827ea178fe52c618d95710383eb77cb88
Joel Lopez <jolopez.satx@gmail.com>
2023-09-11 23:31:44 -0500

    origin main

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 // GIVEN I am taking a code quiz
+
+//getting a handle is always first
+    //create variables for everything needed
+    //identify them in html document.getElementByID etc 
+
+
+//getting started with the basics
     //Create a quiz in javascript, HTML & CSS could be basic
     //html could contain body and divs
+    //How can we add questions and answers? Arrays, objects, var questions: etc
+    //ex question : what color is the sun
+    //question must be included
+    //answers must be included
+    //correct answer must be given
+
+//Time variables
+    //var timeLeft = 60 seconds etc
+    //var timerInterval
+    //var score = number
+
 
 // WHEN I click the start button
+    //create a function to start the quiz
     //must use click eventListener, "click"
     //must create button for that
+
 // THEN a timer starts and I am presented with a question
     //must create a function with interval, increments??, decrements??, timer does not stop??
+    //if timeLeft<=0 and ||  all questions are answered, end quiz
+
 // WHEN I answer a question
     //there will be a questions with multiple answers (TF? Multiple Choice? Both?)
 // THEN I am presented with another question
