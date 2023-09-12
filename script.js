e871948c95d27cd4c4e56c3e520656fe54de65a4
Joel Lopez <jolopez.satx@gmail.com>
2023-09-11 23:15:42 -0500

    origin main

---
+++ script.js
@@ -0,0 +1,32 @@
+// GIVEN I am taking a code quiz
+    //Create a quiz in javascript, HTML & CSS could be basic
+    //html could contain body and divs
+
+// WHEN I click the start button
+    //must use click eventListener, "click"
+    //must create button for that
+// THEN a timer starts and I am presented with a question
+    //must create a function with interval, increments??, decrements??, timer does not stop??
+// WHEN I answer a question
+    //there will be a questions with multiple answers (TF? Multiple Choice? Both?)
+// THEN I am presented with another question
+    //Next question will apear when I click on answer. 
+    //function navigate(direction)??
+    //propogation??
+    //event.listener click
+// WHEN I answer a question incorrectly
+    //if correct....else...
+    //click on incorrect answer
+
+// THEN time is subtracted from the clock
+    //else deduct time from timer
+// WHEN all questions are answered or the timer reaches 0
+    //if time reaches zero, game over
+    //if all questions =complete, game over
+// THEN the game is over
+    //game will be over
+// WHEN the game is over
+    //if game is over
+// THEN I can save my initials and my score
+    //save scores
+    //localStorage.setItem
