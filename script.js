04dcd817eb8e88bee292528c9b98f32936ec7409
Joel Lopez <jolopez.satx@gmail.com>
2023-09-12 23:21:57 -0500

    origin main

--- script.js
+++ script.js
@@ -2,40 +2,79 @@
 
 //getting a handle is always first
     //create variables for everything needed
-    //identify them in html document.getElementByID etc 
+var timerCount;
+var timeLeft;
+var testDone;
+var timeInterval;
+var score;
 
+
+    //identify them in html document.getElementByID etc 
+var startButton = document.querySelector(".start-button");
+var timerElement = document.querySelector(".timer-count")
 
 //getting started with the basics
     //Create a quiz in javascript, HTML & CSS could be basic
     //html could contain body and divs
     //How can we add questions and answers? Arrays, objects, var questions: etc
+    var Questions =[];
+    var Answers =[];
     //ex question : what color is the sun
     //question must be included
     //answers must be included
     //correct answer must be given
 
-//Time variables
-    //var timeLeft = 60 seconds etc
-    //var timerInterval
-    //var score = number
 
 
 // WHEN I click the start button
     //create a function to start the quiz
+    function startQuiz(){
+
+
+
+    }
     //must use click eventListener, "click"
+    startButton.addEventListener("click", startQuiz);
+
     //must create button for that
 
 // THEN a timer starts and I am presented with a question
     //must create a function with interval, increments??, decrements??, timer does not stop??
     //if timeLeft<=0 and ||  all questions are answered, end quiz
+    //Time variables
+    //var timeLeft = 60 seconds etc
+    //var timerInterval
+    // Timer that counts down 
+
+  
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    //questions.length = the number of questions.
+    function startTimer(){
+        timerInterval = setInterval(function() {
+            timeLeft--;
+            timeRemaining.textContent = timeLeft;
+            if (timeLeft <= 0 || currentQuestionIndex === questions.length) {
+                endQuiz();
+            }
+        }, 1000);
+    }
 
 // WHEN I answer a question
     //there will be a questions with multiple answers (TF? Multiple Choice? Both?)
+function selectAnswer(){
+
+
+}
 // THEN I am presented with another question
     //Next question will apear when I click on answer. 
     //function navigate(direction)??
     //propogation??
     //event.listener click
+function nextQuestion(){
+
+
+    
+}
 // WHEN I answer a question incorrectly
     //if correct....else...
     //click on incorrect answer
